b84db1d44a74c3d6c38125c3eb5fde6925c96bb7
webNekit <mochalov-hunter@mail.ru>
2025-12-30 00:08:16 +0300

    prisma: create module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.enableCors({
+    credentials: true,
+    origin: configService.getOrThrow<string>('CORS_ORIGIN'),
+  });
+
+  app.use(cookieParser);
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    defaultVersion: 'v1',
+    type: VersioningType.URI,
+  });
+  await app.listen(process.env.PORT ?? 9000);
+
+  // log
+  console.log('Applications is running on: http://localhost:9000/api/v1');
 }
+
 bootstrap();
